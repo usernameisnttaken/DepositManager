9bb41ffe5db1d447801629a22ceae7d42704dc27
EFedoseev <efedoseev@ibs.ru>
2020-03-23 19:49:27 +0500

    data.dql changerd: merge into instead of insert into

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,14 @@
-/*
-INSERT INTO banks (id, name, bic)
+MERGE INTO banks
 VALUES (1, 'Sberbank', '000000000'),
        (2, 'VTB', '111111111'),
        (3,  'Tinkoff', '222222222');
 
-INSERT INTO clients (id, name, short_name, address, legal_form)
+MERGE INTO clients
 VALUES (1, 'client1', 'client1', 'address1', 'ooo'),
        (2, 'client2', 'client2', 'address2', 'zao'),
        (3, 'client3', 'client3', 'address3', 'pao');
 
-INSERT INTO deposits (id, client_id, bank_id, opening_date, percent, period)
+MERGE INTO deposits
 VALUES (1, 1, 1, current_timestamp(), 10, 12),
        (2, 2, 2, current_timestamp(), 11, 11),
        (3, 3, 3, current_timestamp(), 15, 24);
-*/
